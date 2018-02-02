b824913a690fc6d2f63f49ecb2277fc9a6019736
chand0048 <chand0048@CA-SU-F106-06.sduhsd.lan>
2018-02-02 14:58:31 -0800

    APCSA-2/2/17-14:58

--- APCSA-Dylan Chan/src/unit1/Lab01a.java
+++ APCSA-Dylan Chan/src/unit1/Lab01a.java
@@ -1,9 +1,39 @@
 package unit1;
 
-public class Lab01a {
-
+public class Lab01a 
+{
+	public void stars()
+	{
+		System.out.println("-------------------------------------------");
+	}
+	
+	public void stripes()
+	{
+		System.out.println("*******************************************");
+	}
+	
+	public void box()
+	{
+		int counter = 0;
+		Lab01a starsAndStripes = new Lab01a();
+		while (counter <= 13)
+		{
+			if (1 == counter % 2)
+			{
+				starsAndStripes.stars();
+			}
+	
+			{
+				starsAndStripes.stripes();
+			}
+			
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		
 
 	}
 
