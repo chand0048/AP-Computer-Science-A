df95f91219cbdc333b0384915d72d1ef387bd065
Dylan <Dylan@192.168.0.103>
2018-02-03 23:12:13 -0800

    APCSA-2/3/18-23:12

--- APCSA-Dylan Chan/src/unit1/Lab01b.java
+++ APCSA-Dylan Chan/src/unit1/Lab01b.java
@@ -1,26 +1,35 @@
 package unit1;
 
-import javax.swing.JFrame;
-import java.awt.Graphics;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public class Lab01b extends JFrame
 {
+
 	public Lab01b()
 	{
 		setSize(640, 480);
-		setBackground(Color.WHITE);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 	}
+	
 	public void paint(Graphics window)
 	{
 		window.setColor(Color.BLACK);
+		window.drawString("It\'s a smiley face!", 280, 100);
+		window.setColor(Color.YELLOW);
+		window.fillOval(170, 140, 300, 300);
+		window.setColor(Color.BLACK);
+		window.fillOval(250, 220, 30, 30);
+		window.fillOval(360, 220, 30, 30);
+		window.drawArc(230, 255, 180, 120, 200, 140);
 	}
+	
 	public static void main(String[] args) 
 	{
-		Lab01b window = new Lab01b();
-
-		
+		Lab01b gui = new Lab01b();
+	
 	}
 
 	
