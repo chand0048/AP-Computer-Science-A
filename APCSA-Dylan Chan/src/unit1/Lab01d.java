210c91b880904aede2bc2c0c634bf78c7d6f6ba0
Dylan <Dylan@192.168.0.103>
2018-02-03 23:59:14 -0800

    APCSA-2/3/18-23:59

---
+++ APCSA-Dylan Chan/src/unit1/Lab01d.java
@@ -0,0 +1,48 @@
+package unit1;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Lab01d extends JPanel
+{
+	JFrame window;
+	
+	public Lab01d()
+	{
+		window = new JFrame();
+		
+		window.add(this);
+		window.setVisible(true);
+		window.setSize(640, 480);
+		window.setBackground(Color.WHITE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	public void paint(Graphics window)
+	{
+		window.setColor(Color.LIGHT_GRAY);
+		window.fillRect(290, 90, 60, 60);
+		window.fillRect(260, 150, 120, 150);
+		
+		window.setColor(Color.DARK_GRAY);
+		window.drawArc(270, 300, 80, 180, 100, 90);
+		window.drawArc(290, 300, 80, 180, 80, -90);
+		window.drawArc(190, 180, 140, 220, 90, 110);
+		window.drawArc(310, 180, 140, 220, 90, -110);
+		
+		window.setColor(Color.GREEN);
+		window.fillOval(309, 105, 22, 30);
+		
+		window.setColor(Color.BLACK);
+		window.fillOval(309, 115, 22, 10);
+		window.drawString("This is my robot!", 275, 70);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		Lab01d robot = new Lab01d();
+	}
+
+}
