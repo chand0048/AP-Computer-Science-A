7611b865e66c576310ed7299f2bd5d76d2140fb3
chand0048 <chand0048@CA-SU-F106-06.sduhsd.lan>
2018-02-06 14:39:05 -0800

    APCSA-2/6/18-14:38

--- APCSA-Dylan Chan/src/unit2/Unit2_ReviewExercises.java
+++ APCSA-Dylan Chan/src/unit2/Unit2_ReviewExercises.java
@@ -1,7 +1,11 @@
 package unit2;
 
+import java.util.Scanner;
+
 public class Unit2_ReviewExercises 
 {
+	double velocity, acceleration;
+	
 	double balance, interestRate;
 	
 	public Unit2_ReviewExercises()
@@ -12,8 +16,38 @@ public class Unit2_ReviewExercises
 	{
 		
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public void promptUser()
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Velocity of Plane: ");
+		velocity = keyboard.nextDouble();
+		
+		System.out.print("Accerleration of Plane: ");
+		acceleration = keyboard.nextDouble();
+		
+		keyboard.close();
+	}
+	
+	public double runwayLength()
+	{
+		promptUser();
+		
+		double length = Math.pow(velocity, 2) / (2 * acceleration);
+		
+		return length;
+		
+	}
+	public static void main(String[] args) 
+	{
+		// Exercise 2
+		Unit2_ReviewExercises run = new Unit2_ReviewExercises();
+		
+		System.out.printf("The minimum runway length is %.3f meters" ,run.runwayLength());
+		
+		
+		
 
 	}
 
