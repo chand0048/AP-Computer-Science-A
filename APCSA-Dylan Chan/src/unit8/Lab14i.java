e7812ec763f12b4ab3d3e03018e3ed137d927335
Dylan <Dylan@192.168.0.104>
2018-02-22 20:39:31 -0800

    APCSA-2/2/18-20:39

--- APCSA-Dylan Chan/src/unit8/Lab14i.java
+++ APCSA-Dylan Chan/src/unit8/Lab14i.java
@@ -1,13 +1,43 @@
 package unit8;
 
-import static java.lang.System.*;
-
 public class Lab14i
 {
 	public static void main( String args[] )
 	{
-		RomanNumerals test = new RomanNumerals("XX");
-		System.out.println(test.getNumber());
+		RomanNumerals test = new RomanNumerals(10);
+		System.out.printf("%s is %s", test.getNumber(), test);
 		
+		test.setNumber(100);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(1000);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(2500);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(1500);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(23);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(38);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setNumber(49);
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setRoman("LXXVII");
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setRoman("XLIX");
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setRoman("XX");
+		System.out.printf("%s is %s", test.getNumber(), test);
+		
+		test.setRoman("XXXVIII");
+		System.out.printf("%s is %s", test.getNumber(), test);
 	}
 }
--- APCSA-Dylan Chan/src/unit8/RomanNumerals.java
+++ APCSA-Dylan Chan/src/unit8/RomanNumerals.java
@@ -1,79 +1,112 @@
 package unit8;
 
-import static java.lang.System.*;
-
 public class RomanNumerals
 {
 	private int number;
 	private String roman;
 
+	private final static int[] sortedNUMBERS= {900,400,90,40,9,4,
+			1000,500,100,50,10,5,1};
+
+	private final static String[] sortedLETTERS = {"CM","CD","XC","XL","IX","IV",
+			  "M","D","C","L","X","V","I"};
+
 	private final static int[] NUMBERS= {1000,900,500,400,100,90,
-													50,40,10,9,5,4,1};
-
+			50,40,10,9,5,4,1};
+	
 	private final static String[] LETTERS = {"M","CM","D","CD","C","XC",
-												  "L","XL","X","IX","V","IV","I"};
-
+			  "L","XL","X","IX","V","IV","I"};
+	
 	public RomanNumerals(String str)
 	{
 		roman = str;
+		number = 0;
 		String temp = str;
-		while (temp.length() > 0)
+		for (int index = 0; index < sortedLETTERS.length; index++)
 		{
-			for (int index = 0; index < LETTERS.length; index++)
+			int psn = 0;
+			while (psn != temp.length())
 			{
-				if (temp.indexOf(LETTERS[index]) > 0)
+				if (temp.indexOf(sortedLETTERS[index], psn) >= 0)
 				{
-					if (LETTERS[index].length() == 2 && temp.length() == 1)
+					psn = temp.indexOf(sortedLETTERS[index]);
+					if (sortedLETTERS[index].length() == 2)
 					{
-						temp = "";
+						temp = temp.substring(0, psn) + temp.substring(psn + 2);
 					}
-					else if(LETTERS[index].length() == 1 && temp.length() == 1)
+					else 
 					{
-						temp = "";
+						temp = temp.substring(0, psn) + temp.substring(psn + 1);
 					}
-					number += NUMBERS[index];
-					if (LETTERS[index].length() == 2)
-					{
-						temp = temp.substring(0, temp.indexOf(LETTERS[index])) + temp.substring(temp.indexOf(LETTERS[index] + 2));
-					}
-					else if(LETTERS[index].length() == 1)
-					{
-						temp = temp.substring(0, temp.indexOf(LETTERS[index])) + temp.substring(temp.indexOf(LETTERS[index] + 1));
-					}
-					number += NUMBERS[index];
+					number += sortedNUMBERS[index];
+				}
+				else
+				{
+					psn++;
 				}
 			}
 		}
 	}
 
-
-	
-
 	public RomanNumerals(Integer num)
 	{
 		number = num;
-
-
-	}
-
-	public void setNumber(Integer num)
-	{
-		number = num;
-
-
-
-
+		roman = "";
+		int temp = num;
+		for (int index = 0; index < NUMBERS.length; index++)
+		{
+			while (temp >= NUMBERS[index])
+			{
+				roman += LETTERS[index];
+				temp -= NUMBERS[index];
+			}
+		}
 	}
 
 	public void setRoman(String str)
 	{
 		roman = str;
-		for (int index = 0; index < roman.length(); index++)
+		number = 0;
+		String temp = str;
+		for (int index = 0; index < sortedLETTERS.length; index++)
 		{
-			
+			int psn = 0;
+			while (psn != temp.length())
+			{
+				if (temp.indexOf(sortedLETTERS[index], psn) >= 0)
+				{
+					psn = temp.indexOf(sortedLETTERS[index]);
+					if (sortedLETTERS[index].length() == 2)
+					{
+						temp = temp.substring(0, psn) + temp.substring(psn + 2);
+					}
+					else 
+					{
+						temp = temp.substring(0, psn) + temp.substring(psn + 1);
+					}
+					number += sortedNUMBERS[index];
+				}
+				else
+				{
+					psn++;
+				}
+			}
 		}
+	}
 
-
+	public void setNumber(Integer num)
+	{
+		number = num;
+		roman = "";
+		int temp = num;
+		for (int index = 0; index < NUMBERS.length; index++)
+		{
+			while (temp >= NUMBERS[index])
+			{
+				roman += LETTERS[index];
+				temp -= NUMBERS[index];
+			}
+		}
 	}
 
 	public Integer getNumber()
