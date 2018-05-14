6e86d7bb5770ca4c5f853979aa96665e30e224f0
chand0048 <chand0048@CA-SU-F106-06.sduhsd.lan>
2018-05-14 14:46:12 -0700

    APCSA-5/14/18-14:46

--- APCSA-Dylan Chan/src/javaProjectTareetria/OpenWorldRunner.java
+++ APCSA-Dylan Chan/src/javaProjectTareetria/OpenWorldRunner.java
@@ -37,8 +37,9 @@ public class OpenWorldRunner extends JFrame
 				+ "\nUse the SCROLL WHEEL to SWITCH between the rifle and the building tool."
 				+ "\nWhen the rifle is selected click LMB TO FIRE. When the building tool is selected "
 				+ "\nclick LMB on where you want to PLACE A BLOCK and click RMB on an existing "
-				+ "\nblock to REMOVE it. Zombies will attack you. Try to survive for as long as "
-				+ "\npossible by using a combination of building and shooting. Good luck!",
+				+ "\nblock to REMOVE it. Zombies will attack you. Falling damge is a thing.\n "
+				+ "Try to survive for as long as possible by using a combination of building\n"
+				+ " and shooting. Good luck!",
 				"Instructions", JOptionPane.OK_OPTION);
 		OpenWorldRunner run = new OpenWorldRunner();
 	}
